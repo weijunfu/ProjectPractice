f8e39726db92d574fe4a5b3ab4d0a828e8271372
ijunfu <ijunfu@163.com>
2022-01-21 23:56:40 +0800

    :fire: 基于Web测试请求返回值

--- 03.SpringBoot/18.test/README.md
+++ 03.SpringBoot/18.test/README.md
@@ -99,3 +99,27 @@ class BookControllerTest {
     }
 }
 ```
+
+### 测试请求返回值
+
+```java
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) // 定义Web环境
+@AutoConfigureMockMvc   // 开启虚拟MVC
+class BookControllerTest {
+    
+    @Test
+    void testBody(@Autowired MockMvc mvc) throws Exception {
+        // 1. 定义请求地址
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/books");
+        // 2. 执行请求
+        ResultActions actions = mvc.perform(builder);
+
+        // 3. 预计本次调用返回值
+        ContentResultMatchers content = MockMvcResultMatchers.content();
+        ResultMatcher ret = content.string("query all books");
+
+        // 4. 与期望值对比
+        actions.andExpect(ret);
+    }
+}
+```
--- 03.SpringBoot/18.test/src/test/java/org/ijunfu/controller/BookControllerTest.java
+++ 03.SpringBoot/18.test/src/test/java/org/ijunfu/controller/BookControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.ContentResultMatchers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.result.StatusResultMatchers;
 
@@ -42,5 +43,20 @@ class BookControllerTest {
 
         // 4. 与期望值对比
         actions.andExpect(ok);
+    }
+
+    @Test
+    void testBody(@Autowired MockMvc mvc) throws Exception {
+        // 1. 定义请求地址
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/books");
+        // 2. 执行请求
+        ResultActions actions = mvc.perform(builder);
+
+        // 3. 预计本次调用返回值
+        ContentResultMatchers content = MockMvcResultMatchers.content();
+        ResultMatcher ret = content.string("query all books");
+
+        // 4. 与期望值对比
+        actions.andExpect(ret);
     }
 }
