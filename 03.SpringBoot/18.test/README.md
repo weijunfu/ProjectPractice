8c06326ee54b68fae12aa6b18ef49a82481f8714
ijunfu <ijunfu@163.com>
2022-01-21 23:10:06 +0800

    :fire: 测试时，使用临时属性（Properties & args）

--- 03.SpringBoot/18.test/README.md
+++ 03.SpringBoot/18.test/README.md
@@ -1,3 +1,26 @@
 # 测试
 
+## 添加临时属性（两种模式）
 
+### 第一种：properties属性模式
+```java
+@SpringBootTest(properties = {"test.author=ijunfu"})
+class ApplicationTests {
+    //...
+}
+```
+
+### 第二种：args属性模式
+> 可用于模拟命令行操作
+
+```java
+@SpringBootTest(args = {"--test.author=ijunfu"})
+class ApplicationTests {
+ // ...   
+}
+```
+
+若，配置文件、properties属性和args属性同时配置相同参数，优先级从低到高为：配置文件 < properties属性 < args属性。
+
+### 优势
+比多环境开发中的测试环境影响范围更小，仅对当前测试类有效。
--- 03.SpringBoot/18.test/src/test/java/org/ijunfu/ApplicationTests.java
+++ 03.SpringBoot/18.test/src/test/java/org/ijunfu/ApplicationTests.java
@@ -5,15 +5,23 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest(properties = {"test.author=ijunfu"})
+@SpringBootTest(args = {"--test.author=ijunfu"})
 class ApplicationTests {
 
     @Value("${test.msg}")
     private String msg;
+
+    @Value("${test.author}")
+    private String author;
 
     @Test
     void testProperties() {
         Assertions.assertEquals("Test Value", msg);
     }
 
+    @Test
+    void testAuthor() {
+        Assertions.assertEquals("ijunfu", author);
+    }
 }
