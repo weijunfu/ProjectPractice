b46b37bac242292af1523542f7891951f79ad153
ijunfu <ijunfu@163.com>
2022-01-21 22:53:29 +0800

    :fire: @Value读取配置

--- 03.SpringBoot/18.test/src/test/java/org/ijunfu/ApplicationTests.java
+++ 03.SpringBoot/18.test/src/test/java/org/ijunfu/ApplicationTests.java
@@ -1,13 +1,19 @@
 package org.ijunfu;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
 
+    @Value("${test.msg}")
+    private String msg;
+
     @Test
-    void contextLoads() {
+    void testProperties() {
+        Assertions.assertEquals("Test Value", msg);
     }
 
 }
