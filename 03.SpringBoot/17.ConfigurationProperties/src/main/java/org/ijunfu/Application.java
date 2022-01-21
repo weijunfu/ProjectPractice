28fe749500a474a59216139bb06b763076e7b1d7
ijunfu <ijunfu@163.com>
2022-01-21 17:25:47 +0800

    :fire: 为第三方绑定属性

--- 03.SpringBoot/17.ConfigurationProperties/src/main/java/org/ijunfu/Application.java
+++ 03.SpringBoot/17.ConfigurationProperties/src/main/java/org/ijunfu/Application.java
@@ -1,5 +1,6 @@
 package org.ijunfu;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import org.ijunfu.config.DBConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,6 +15,8 @@ public class Application {
         DBConfig dbConfig = context.getBean(DBConfig.class);
         System.out.println(dbConfig);
 
+        DruidDataSource dataSource = context.getBean(DruidDataSource.class);
+        System.out.println(dataSource);
     }
 
 }
---
+++ 03.SpringBoot/17.ConfigurationProperties/src/main/java/org/ijunfu/config/DruidConfig.java
@@ -0,0 +1,39 @@
+package org.ijunfu.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @Title          Druid 数据库连接池
+ * @Description    <TODO>
+ *
+ * @author weijunfu<ijunfu @ 1 6 3 . com>
+ * @date 2022/01/21 17:05
+ * @version 1.0.0
+ *
+ */
+
+@Configuration
+public class DruidConfig {
+
+    /**
+     *
+     * @Title       dataSource
+     * @Description 为第三方绑定属性
+     *
+     * @author      weijunfu<ijunfu@163.com>
+     * @date        2022/01/21 17:20
+     * @version     1.0.0
+     * @param
+     * @Return      com.alibaba.druid.pool.DruidDataSource
+     */
+    @Bean
+    @ConfigurationProperties(prefix = "datasource")
+    public DruidDataSource dataSource() {
+        DruidDataSource dataSource = new DruidDataSource();
+        return dataSource;
+    }
+}
