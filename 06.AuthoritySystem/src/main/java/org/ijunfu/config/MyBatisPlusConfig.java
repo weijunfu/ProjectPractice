db3e8feb6785900b213348793e9e93de33e7edcb
ijunfu <ijunfu@163.com>
2022-02-05 00:29:43 +0800

    :sparkles: 分页插件

---
+++ 06.AuthoritySystem/src/main/java/org/ijunfu/config/MyBatisPlusConfig.java
@@ -0,0 +1,31 @@
+package org.ijunfu.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @Title          <Title>
+ * @Description    <TODO>
+ *
+ * @author weijunfu<ijunfu @ 1 6 3 . com>
+ * @date 2022/02/05 00:25
+ * @version 1.0.0
+ *
+ */
+
+@Configuration
+public class MyBatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+
+        // 分页插件
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+
+        return interceptor;
+    }
+}
